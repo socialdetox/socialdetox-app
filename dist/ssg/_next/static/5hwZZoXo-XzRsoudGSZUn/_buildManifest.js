437da564feecf7aaffeab1881c0885bcbbb25bf6
SocialDetox.ai <181064963+socialdetox@users.noreply.github.com>
2024-10-31 17:27:39 +0200

    [update] Version 0.0.28
    # Thu Oct 31 17:26:00 2024 +0200
    [code] Username ellipsised
    
    # Thu Oct 31 17:21:33 2024 +0200
    [code] Secondary page cleanup

---
+++ dist/ssg/_next/static/5hwZZoXo-XzRsoudGSZUn/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@socialdetox.ai>
+ * @copyright © 2024 SocialDetox.ai https://socialdetox.ai
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,e){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,"static/chunks/60-f3d847e77eb181a7.js",e,"static/chunks/pages/index-9c50c4f0f7d3b939.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":["static/chunks/183-b7f83caecfe07630.js",e,"static/chunks/pages/login-183613f24e900430.js"],"/profile":[s,"static/chunks/504-471061d10a03b28a.js","static/chunks/pages/profile-377ba7198a0dcc2d.js"],sortedPages:["/","/401","/_app","/_error","/_offline","/login","/profile"]}}("static/chunks/149-7a8e5ebbb258aa96.js","static/chunks/655-fecaca0b61d858bf.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
