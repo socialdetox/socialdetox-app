4506770822b3462129b502489ad91fe81df89315
Oglama <181064963+oglama@users.noreply.github.com>
2024-12-19 18:48:48 +0200

    [update] Version 0.0.31
    # December 19, 2024 at 6:38 PM
    [code] Hidden overflow for sparkles effect
    
    # December 19, 2024 at 6:29 PM
    [code] Updated logs behavior
    
    # December 19, 2024 at 5:47 PM
    [code] Fixed logs init
    
    # December 19, 2024 at 5:26 PM
    [code] Persistent logs UI
    
    # December 19, 2024 at 11:55 AM
    [code] More efficient logging
    
    # December 18, 2024 at 9:12 PM
    [code] Rate limiting on log dom changes
    
    # December 18, 2024 at 8:09 PM
    [code] Moved logs to session storage
    
    # December 18, 2024 at 4:04 PM
    [code] Cleaner toggle
    
    # December 18, 2024 at 3:20 PM
    [code] Moved light mode
    
    # December 18, 2024 at 11:08 AM
    [code] Improved Agent Logs

---
+++ dist/ssg/_next/static/M6nvhr1A3WeNHQX9m0KLp/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@oglama.com>
+ * @copyright © 2024 Oglama https://oglama.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s){return{__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":[s,"static/chunks/839-c556e89fb49f6f46.js","static/chunks/pages/index-1987da86c2504e9e.js"],"/401":["static/chunks/pages/401-83d7d5cf2d8e5ac4.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/_offline":["static/chunks/pages/_offline-34198a61a2499178.js"],"/login":["static/chunks/761-6740d46230cdeb67.js","static/chunks/pages/login-602cbb240878fc21.js"],"/profile":[s,"static/chunks/604-454d4d23465590ad.js","static/chunks/pages/profile-7511b7e9b119cf96.js"],sortedPages:["/","/401","/_app","/_error","/_offline","/login","/profile"]}}("static/chunks/517-500e0e84df4c7e35.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
